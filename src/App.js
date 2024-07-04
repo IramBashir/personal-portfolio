01d12bf60532da45527674a412a140f4f2e074b0
IramBashir <irambashir889@gmail.com>
2024-07-04 04:55:22 -0700

    New changes to deploy
    New changes to deploy

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Home from './components/Home';
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
@@ -17,12 +17,12 @@ const App = () => {
       <GlobalStyles />
       <Router>
         <Header />
-        <Switch>
+        <Routes>
           <Route path="/" exact component={Home} />
           <Route path="/about" component={About} />
           <Route path="/projects" component={Projects} />
           <Route path="/contact" component={Contact} />
-        </Switch>
+        </Routes>
         <Footer />
       </Router>
     </ThemeProvider>
