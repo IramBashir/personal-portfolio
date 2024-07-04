adbf977f788e3e1d6fb731636a03b0a3dd2eec28
IramBashir <irambashir889@gmail.com>
2024-07-04 10:37:19 -0700

    Updated header of the site with tailwind
    Updated header of the site with tailwind

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,40 +1,48 @@
 // src/components/Header.js
-import React from 'react';
-import styled from 'styled-components';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <HeaderWrapper>
-      <h1>Iram Bashir</h1>
-      <nav>
-        <ul>
-          <li><Link to={`${process.env.PUBLIC_URL}/`}>Home</Link></li>
-          <li><Link to={`${process.env.PUBLIC_URL}/about`}>About</Link></li>
-          <li><Link to={`${process.env.PUBLIC_URL}/projects`}>Projects</Link></li>
-          <li><Link to={`${process.env.PUBLIC_URL}/contact`}>Contact</Link></li>
-        </ul>
-      </nav>
-    </HeaderWrapper>
+    <header className="bg-blue-600 p-4 shadow-md">
+      <div className="container mx-auto flex justify-between items-center">
+        <h1 className="text-white text-2xl font-bold">Iram Bashir</h1>
+        <nav className="hidden md:flex space-x-6">
+          <Link to={`${process.env.PUBLIC_URL}/`} className="text-white hover:text-blue-200 transition duration-300">Home</Link>
+          <Link to={`${process.env.PUBLIC_URL}/about`} className="text-white hover:text-blue-200 transition duration-300">About</Link>
+          <Link to={`${process.env.PUBLIC_URL}/projects`} className="text-white hover:text-blue-200 transition duration-300">Projects</Link>
+          <Link to={`${process.env.PUBLIC_URL}/contact`} className="text-white hover:text-blue-200 transition duration-300">Contact</Link>
+        </nav>
+        <div className="md:hidden">
+          <button onClick={() => setIsOpen(!isOpen)} className="text-white focus:outline-none">
+            <FontAwesomeIcon icon={isOpen ? faTimes : faBars} className="h-6 w-6 transition duration-300" />
+          </button>
+        </div>
+      </div>
+      {isOpen && (
+        <nav className="md:hidden bg-blue-600 p-4 transition duration-300">
+          <ul className="space-y-4">
+            <li>
+              <Link to={`${process.env.PUBLIC_URL}/`} className="text-white hover:text-blue-200 block transition duration-300">Home</Link>
+            </li>
+            <li>
+              <Link to={`${process.env.PUBLIC_URL}/about`} className="text-white hover:text-blue-200 block transition duration-300">About</Link>
+            </li>
+            <li>
+              <Link to={`${process.env.PUBLIC_URL}/projects`} className="text-white hover:text-blue-200 block transition duration-300">Projects</Link>
+            </li>
+            <li>
+              <Link to={`${process.env.PUBLIC_URL}/contact`} className="text-white hover:text-blue-200 block transition duration-300">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      )}
+    </header>
   );
 };
-
-const HeaderWrapper = styled.header`
-  background-color: ${(props) => props.theme.colors.primary};
-  padding: ${(props) => props.theme.spacing.medium};
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-
-  h1 {
-    color: white;
-  }
-
-  nav a {
-    color: white;
-    margin-left: ${(props) => props.theme.spacing.large};
-    text-decoration: none;
-  }
-`;
 
 export default Header;
